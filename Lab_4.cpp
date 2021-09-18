7733db7047871a17400ffa121a1e80cc966e1d58
KochenovEvg <kochenovevgene087@gmail.com>
2021-09-18 23:50:23 +0300

    lab_4_end

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -3,7 +3,34 @@
 using namespace std;
 int main()
 {
-  
-  system("pause");
-  return 0;
+    int k, n, i;
+    cout << "Enter length of array" << endl;
+    cin >> k;
+    cout << "How much to shift?" << endl;
+    cin >> n;
+//начало создания динамического массива
+    int *arr = new int[k];
+    int *marr = new int[k];
+//конец создания динамического массива
+    for (i = 0; i < k; i++)
+    {
+        arr[i] = rand() % 100 + 1;
+        cout << "  " << arr[i] << "  ";
+    }
+    cout << endl;
+//Сдвиг
+    for (i = 0; i < k; i++)
+    {
+        if (i + n < k)
+            marr[i] = arr[i + n];
+        else
+            marr[i] = arr[abs(i + n - k)];
+        cout << "  " << marr[i] << "  ";
+    }
+    cout << endl;
+//Сдвиг
+    delete [] arr;
+    delete [] marr;
+    system("pause");
+    return 0;
 }
