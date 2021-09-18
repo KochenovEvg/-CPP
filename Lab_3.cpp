953eb2f06b2fd5a580b9e4ff1557df157a431c6b
KochenovEvg <kochenovevgene087@gmail.com>
2021-09-18 22:02:37 +0300

    Lab_3_end

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -15,29 +15,29 @@ float func(float x)
  }
 int main()
 {
-    float a, b, h, x, s, m;
+    float a, b, h, x, m;
     int i, n, j;
-    cout.precision(3);
+    cout.precision(5);
     a = 0.1;
     b = 1;
     h = (b - a)/10;
-    cout<<"Values of F(x) from 0.1 to 1.0: "<<endl;
+    cout<<"Values of F(x) from 0.1 to 1.0: with h=(b-a)/10="<< h <<endl;
     for (x = 0.1; x <= b + h; x = x + h)
     {
         cout <<"F("<<x<<")="<<func(x)<<";"<<endl;
     }
-    n = 8;
+    n = 10;
     cout << "Row:"<<endl;
-    cout << "fact(5)" << fact(5) << endl;
-    m = 0;
-    for(x = 0.1; x <= b; x = x + h)
+    for(x = 0.1; x <= b + h; x = x + h)
     {
+      m = 0;
       for(i = 0; i <= n; i++)
       {
-        m = m + (pow(x, (2 * n + 1))/fact(2 * n + 1));
+        m = m + (pow(x, (2 * i + 1))/fact(2 * i + 1));
       }
-      cout << m <<"; ";
+      cout << "F("<< x << ")=" << m << "; " << endl;
     }
+
     system("pause");
     return 0;
 }
