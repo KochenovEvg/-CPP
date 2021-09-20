586a17a8593997ba875b4a38cf0ed10eed00fd2d
KochenovEvg <kochenovevgene087@gmail.com>
2021-09-20 20:55:34 +0300

    lab_5_0.2

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -3,5 +3,30 @@
 using namespace std;
 int main()
 {
-  
+  int i, j, n, m;
+  cout << "Enter N" << endl;
+  cin >> n;
+  cout << "Enter M" << endl;
+  cin >> m;
+  //array
+  char **arr;
+    arr = new char*[n];
+      for (i = 0; i < n; i++)
+        arr[i] = new char[m];
+  //array
+  for(i = 0; i < n; i++)
+    {
+      for(j = 0; j < m; i++)
+      {
+        arr[i][j] = rand() %90 +65;
+        cout << arr[i][j] << "  ";
+      }
+      cout << endl;
+    }
+  for(i = 0; i < n; i++)
+    delete []arr[i];
+  delete []arr;
+  arr = NULL;
+system("pause");
+return 0;
 }
