afce6acfaffe1a295e0946be0ba625451925ac48
KochenovEvg <kochenovevgene087@gmail.com>
2021-10-03 00:21:36 +0300

    Lab_6_line_end

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 int main()
 {
-  
+  int i, j, t, p, v, q;
+  char str[50];
+  srand(time(0));
+  //t — length of str
+  t = 1 + rand() % 50;
+  cout << "length of line = " << t << endl;
+  //p — переменная рандома
+  for(i = 0; i <= t; i++)
+  {
+    p = 1 + rand() % 3;
+    switch(p)
+    {
+      case 1:
+      str[i] = rand() % 4 + 43;
+      cout << str[i];
+      case 2:
+      str[i] = rand() % 10 + 48;
+      cout << str[i];
+      case 3:
+      str[i] = rand() % 26 + 65;
+      cout << str[i];
+    }
+  }
+  cout << endl;
   system("pause");
   return 0;
 }
+
+//case 1:
+//str[i] = rand() % 4 + 43;
+//case 2:
+//str[i] = rand() % 10 + 48;
+//case 3:
+//str[i] = rand() % 26 + 65;
