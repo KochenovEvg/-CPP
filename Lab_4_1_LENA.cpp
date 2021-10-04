1642b2908727954e5c72562ea0345546d327062f
KochenovEvg <kochenovevgene087@gmail.com>
2021-10-04 17:53:16 +0300

    VAR1

--- Lab_4_1_LENA.cpp
+++ Lab_4_1_LENA.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
   srand(time(0));
-  int i, t, k = 0;
+  int i, t, j, p = 0;
   char str[100];
-  char q = 0;
+  char q;
   cout << "Enter the length of the line: ";
   cin >> t;
   for (i = 0; i < t; i++)
@@ -15,16 +15,16 @@ int main()
     cout << str[i];
   }
   cout << endl;
-  for(i = 0; i < t; i++)
-  {
-    if(str[i] <= 57 && str[i] >= 48)
+    for(i = t; i > 0; i--)
     {
-      q = str[i];
-      str[i] = str[k];
-      str[k] = q;
-      k++;
+      if(str[i] <= 57 && str[i] >= 48)
+      {
+        if(str[i+1]<= 57 && str[i+1] >= 48) continue;
+        q = str[i-1];
+        str[i-1] = str [i];
+        str[i] = q;
+      }
     }
-  }
   for(i = 0; i < t; i++)
   {
     cout << str[i];
