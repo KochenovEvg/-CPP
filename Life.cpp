74d20d8bf5040fc52f088626ad97849d979314b2
KochenovEvg <kochenovevgene087@gmail.com>
2021-10-09 23:52:51 +0300

    SNAKE_0.1

---
+++ Life.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<ctime>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+  int i, j, n = 40, m = 40;
+  char** arr = new char*[n];
+  for(i = 0; i < n; i++)
+  arr[i] = new char[m];
+
+
+
+  system("pause");
+  return 0;
+}
