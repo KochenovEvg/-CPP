76a8a6d4c8db41478a25e3c787b000eec8cdcf08
KochenovEvg <kochenovevgene087@gmail.com>
2021-10-09 22:25:59 +0300

    Lab_8_end

--- Lab_8.cpp
+++ Lab_8.cpp
@@ -24,37 +24,30 @@ float func(float x)
 
 float row(float x, float e)
 {
-    float r, p, k;
+    float r, p, k, s;
     r =(x*x)/8.0;
-    cout << "r" << 2 <<" = " << r << endl;
+    s = r;
     for(k = 3; abs(r) > e ; k++)
     {
       p = -(x*(2 * k - 3)/(2*k));
       r = r*p;
-      cout << "p" << k <<" = " << p << "\t";
-      cout << "r" << k <<" = " << r << endl;
+      s = s + r;
     }
-    return r;
+    return s;
 }
 int main()
 {
-  float e = 0.0001, h = 0.1, a = -0.9, b = 0.9, i;
+  float e = 0.001, h = 0.1, a = -0.9, b = 0.9, i;
   cout << "a = " << a << endl;
   cout << "b = " << b << endl;
   cout << "h = " << h << endl;
   cout << "E = " << e << endl;
-  cout << "F(x): " << endl;
+  cout << "F(x): " << "\t\t\t\t" << "ROW:" <<endl;
   for(i = a; i < b + h; i = i + h)
   {
-    cout << "F(" << i << ") = " << func(i) << endl;
+    cout << "F(" << i << ") = " << func(i)<< "\t\t" <<"Row("<< i <<") = "<<row(i,e) << endl;
   }
-
-  cout << "ROW: " << endl;
-  // for(i = a; i < b + h; i = i + h)
-  // {
-  //   cout << "Row(" << i << ") = "<< row(i,e) << endl;
-  // }
-  cout << "Row(0.9)" << row(0.9, e);
+  cout << endl;
   return 0;
   system("pause");
 }
